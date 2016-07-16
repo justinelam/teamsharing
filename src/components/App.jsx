d345f3486a426e1f3ac9b0d5e71069c980d5ccba
Justine Lam <justine.lam@gmail.com>
2016-07-16 13:01:02 -0700

    Add all views and package react-router and json file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,10 @@ import Person from './Person.jsx';
 import Header from './Header.jsx';
 import Nav from './Nav.jsx';
 import NotFound from './NotFound.jsx';
+import Questions from './Questions.jsx';
+import AddQuestion from './AddQuestion.jsx';
+import Question from './Question.jsx';
+import Vote from './Vote.jsx';
 
 var ReactRouter = require('react-router');
 var Router  = ReactRouter.Router;
@@ -18,7 +22,6 @@ var App = React.createClass({
       <div>
         <Header />
         <Nav />
-        <Person />
       </div>
     )
   }
@@ -28,8 +31,15 @@ var routes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}/>
     <Route path="/person/:name/:id" component={Person}/>
+    <Route path="/questions" component={Questions}/>
+    <Route path="/questions/new" component={AddQuestion}/>
+    <Route path="/questions/submit" component={AddQuestion}/>
+    <Route path="/questions/vote" component={Vote}/>
+    <Route path="/questions/:id" component={Question}/>
     <Route path="*" component={NotFound}/>
   </Router>
 )
 
 ReactDOM.render(routes, document.getElementById('main'));
+
+console.log($);
